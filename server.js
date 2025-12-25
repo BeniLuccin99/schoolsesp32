fd1f2518fb9cff5894d42889d2249fdf2758fb6b
BeniLuccin99 <beniluccin@gmail.com>
2025-12-25 12:28:12 +0200

    esp32 backend

---
+++ server.js
@@ -0,0 +1,23 @@
+// server.js
+import express from 'express';
+import bodyParser from 'body-parser';
+const app = express();
+const PORT = "5000";
+
+app.use(bodyParser.json());
+
+let sensorData = {};
+
+app.post('/api/sensor/data', (req, res) => {
+  sensorData = req.body; // Save latest data
+  console.log('Received from ESP32:', sensorData);
+  res.status(200).send({status: 'ok'});
+});
+
+app.get('/api/sensor/data', (req, res) => {
+  res.json(sensorData); // Serve latest data
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
